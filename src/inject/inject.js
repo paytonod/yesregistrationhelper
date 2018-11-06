9a9467e94fc7fc34feb9a4e113992fc3dacad44b
dasaria <aneesha.dasari@gmail.com>
2018-11-06 15:30:57 -0600

    buttons appear and hooked to getbooks button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,7 @@ chrome.extension.sendMessage({}, function (response) {
 	var readyStateCheckInterval = setInterval(function () {
 		if (document.readyState === "complete") {
 			clearInterval(readyStateCheckInterval);
-			buttons = document.getElementsByClassName("event eventEnrolled");	
+		
 			// ----------------------------------------------------------
 			// This part of the script triggers when page is done loading
 			console.log("Hello. This message was sent from scripts/inject.js");
@@ -11,3 +11,4 @@ chrome.extension.sendMessage({}, function (response) {
 		}
 	}, 10);
 });
+
--- src/inject/scheduleContentScript.js
+++ src/inject/scheduleContentScript.js
@@ -1,20 +1,87 @@
-//gets all class elements
-let buttons = document.getElementsByClassName("event eventEnrolled");
+window.onload = function() {
+    var readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === "complete") {
 
-//creates three new buttons and adds to bottom
-let newBut = document.createElement("BUTTON");
-let newText = document.createTextNode("drop this");
-newBut.appendChild(newText);
-document.body.appendChild(newBut);
+            let buttons = document.getElementsByClassName("event eventEnrolled");
+            for (var i = 0, len = buttons.length; i<len; i++)
+            {
+                let newBut2 = document.createElement("BUTTON");
+                let newText2 = document.createTextNode("--");
+                newBut2.appendChild(newText2);
 
-let newBut2 = document.createElement("BUTTON");
-let newText2 = document.createTextNode("-");
-newBut2.appendChild(newText2);
-document.body.appendChild(newBut2);
+                newBut2.style.backgroundColor = "red";
+                newBut2.style.borderColor = "red";
+                newBut2.style.color = "white";
+                newBut2.style.width = '20px';
+                newBut2.style.height = '12px';
+                newBut2.style.verticalAlign = "middle";
+                newBut2.style.alignItems = "right";
+                newBut2.style.fontSize = '12px';
+                newBut2.style.textAlign = "center";
 
-let newBut3 = document.createElement("BUTTON");
-let newText3 = document.createTextNode("-");
-newBut3.appendChild(newText3);
-document.body.appendChild(newBut3);
+                if (buttons.item(i).children.length < 1)
+                {
+                    document.body.appendChild(newBut2);
+                    buttons.item(i).appendChild(newBut2);
 
+                    newBut2.onmouseover = function() {
+                        newBut2.style.cursor = "pointer";
+                    }
 
+                    newBut2.onclick = function(event) {
+                        event.stopPropagation();
+                        console.log("clicked!");
+                        let classNum = newBut2.parentElement.id.split("_")[2];
+                        console.log(classNum);
+
+                        // let listPage = new XMLHttpRequest();
+                        // function reqListener() {
+                        //     console.log(this.responseXML.title);
+                        // }
+                        // listPage.addEventListener("load", reqListener);
+                        // listPage.open("GET", "SearchClasses!input.action");
+                        // listPage.responseType = "document";
+                        // listPage.send(document); 
+
+                        let classList = document.getElementById("EnrolledClassesList_div").children;
+                        let checkbox;
+                        for (var i = 0, len = classList.length; i < len; i++)
+                        {
+                            let classSel = classList.item(i).tBodies.item(0).children.item(2).children.item(0);
+                            console.log(classSel.id.split("_"));
+                            if (classSel.id.split("_")[1] == classNum)
+                            {
+                                checkbox = classList.item(i).tBodies.item(0).children.item(2).cells[0];
+                                i+=len;
+                            }
+                        }
+
+                        //click Order Books instead of drop
+                        var submit = document.getElementById("bookNowIndicator").getElementsByClassName("first-child").item(0).children.item(0);
+                        console.log(submit);
+                        submit.click();
+                    }
+                }
+            }
+        }
+    }, 10);
+}
+
+// window.onload = function() {
+//     while (document.readyState != "complete"){
+//         console.log("not ready");
+//     }
+//     let buttons = document.getElementsByClassName("event eventEnrolled");
+//     for (var i = 0, len = buttons.length; i<len; i++)
+//     {
+//         console.log("add");
+//         let newBut2 = document.createElement("BUTTON");
+//         let newText2 = document.createTextNode("-");
+//         newBut2.appendChild(newText2);
+//         if (buttons.item(i).children.length < 1)
+//         {
+//             document.body.appendChild(newBut2);
+//             buttons.item(i).appendChild(newBut2);
+//         }
+//     }
+// }
