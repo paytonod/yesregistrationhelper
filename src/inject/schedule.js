b0d31e10dc17fb53d06b2a95b91112be5f1a2d68
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-11-07 22:16:55 -0600

    added some bullshit and a validity check for the document URI

--- src/inject/schedule.js
+++ src/inject/schedule.js
@@ -1,31 +1,39 @@
+// Send message via the extension
 chrome.extension.sendMessage({}, function (response) {
-    var readyStateCheckInterval = setInterval(function () {
-        if (document.readyState === "complete") {
+    let readyStateCheckInterval = setInterval(function () {
+        // Check document state and URI for correct page
+        if (document.readyState === "complete" && document.documentURI.find("SearchClasses")) {
+            // Clear interval
             clearInterval(readyStateCheckInterval);
-            // ----------------------------------------------------------
-            // This part of the script triggers when page is done loading
-            console.log("Hello. This message was sent from scripts/inject.js");
 
+            // Log when current page is done loading
+            console.log("Page is done loading pre-injection");
+
+            // Create div for schedule
             let wrapper = document.createElement('div');
-
             wrapper.setAttribute("id", "wrapper");
             wrapper.style.borderStyle = "inset";
             wrapper.style.borderColor = "grey";
             wrapper.style.overflow = "scroll";
             wrapper.style.height = "400px";
 
+            // Create iFrame to house schedule div
             let iframe = document.createElement('iframe');
-
-            iframe.style.width = "100%";
-            iframe.style.height = "1000%";
+            iframe.style.width = "120%";
+            iframe.style.height = "250%";
             iframe.style.marginLeft = "-45px";
             iframe.style.marginTop = "-300px";
             iframe.style.overflow = "hidden";
-
+            iframe.style.scrolling = "no";
             iframe.src = 'https://acad.app.vanderbilt.edu/more/GetSchedule!input.action#scheduleTable';
+
+            let scheduleDoc = new Document();
+            scheduleDoc.domain = 'https://acad.app.vanderbilt.edu/more/GetSchedule!input.action';
+            let scheduleDiv = scheduleDoc.getElementById("scheduleTable");
+
+            // Append wrapper div to page
             document.body.appendChild(wrapper);
-            (document.getElementById('wrapper')).appendChild(iframe);
-            // ----------------------------------------------------------
+            document.getElementById('wrapper').appendChild(iframe);
         }
     }, 0);
 });
