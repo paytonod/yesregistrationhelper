fcf5bed3699c4efe1ed02ec4bee39ab742300d63
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-11-13 10:42:59 -0600

    fixed error

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,6 +7,5 @@
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function (request, sender, sendResponse) {
-    chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
