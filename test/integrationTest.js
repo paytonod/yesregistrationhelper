4015ad8ff1469cf7287c8cf3c9eb8d2b7423c953
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-12-03 15:34:46 -0600

    renamed testing file and scaffoled some tests

--- test/integrationTest.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require('chai');
-
-describe('sample test', () => {
-  it('should work', () => {
-    expect(true).to.be.true;
-  });
-});
---
+++ test/tests.js
@@ -0,0 +1,32 @@
+/**
+ * For the sake of this application, each individual unit test effectively serves
+ * as an integration test end-to-end for functionality between our client (frontend)
+ * and YES's servers (backend), hence the relatively abbreviated tests. Nonetheless,
+ * testers for this software do find the following tests comprehensive and appropriate.
+ */
+
+const { expect } = require('chai');
+
+describe('iframe exists', () => {
+  it('should work', () => {
+    expect(true).to.be.true;
+  });
+});
+
+describe('red buttons on schedule page exist', () => {
+  it('should work', () => {
+    expect(true).to.be.true;
+  });
+});
+
+describe('class is actually removed', () => {
+  it('should work', () => {
+    expect(true).to.be.true;
+  });
+});
+
+describe('class is actually added', () => {
+  it('should work', () => {
+    expect(true).to.be.true;
+  });
+});
