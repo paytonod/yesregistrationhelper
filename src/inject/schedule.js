ecf8ee535ec86bb72a05bb3434dfa34bdea291d6
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-12-03 14:37:57 -0600

    lints

--- src/inject/schedule.js
+++ src/inject/schedule.js
@@ -1,13 +1,13 @@
 chrome.extension.sendMessage({}, (response) => {
   const readyStateCheckInterval = setInterval(() => {
-    if (document.readyState === "complete") {
+    if (document.readyState === 'complete') {
       clearInterval(readyStateCheckInterval);
 
       // if the schedule is not already on the page, and you're on a YES page, add it
       if (
-        document.getElementById("wrapper") === null &&
-        document.getElementsByTagName("h1")[2] !== undefined &&
-        document.getElementsByTagName("h1")[2].innerText === "Enrolled"
+        document.getElementById('wrapper') === null
+        && document.getElementsByTagName('h1')[2] !== undefined
+        && document.getElementsByTagName('h1')[2].innerText === 'Enrolled'
       ) {
         showSchedule();
       }
@@ -16,29 +16,29 @@ chrome.extension.sendMessage({}, (response) => {
 
   let showSchedule = () => {
     // create a div to hold the schedule
-    const wrapper = document.createElement("div");
+    const wrapper = document.createElement('div');
 
-    wrapper.setAttribute("id", "wrapper");
-    wrapper.style.borderStyle = "inset";
-    wrapper.style.borderColor = "grey";
-    wrapper.style.overflow = "hidden";
-    wrapper.style.height = "400px";
-    wrapper.scrolling = "no";
+    wrapper.setAttribute('id', 'wrapper');
+    wrapper.style.borderStyle = 'inset';
+    wrapper.style.borderColor = 'grey';
+    wrapper.style.overflow = 'hidden';
+    wrapper.style.height = '400px';
+    wrapper.scrolling = 'no';
 
     // create an iframe to show the schedule
-    const iframe = document.createElement("iframe");
+    const iframe = document.createElement('iframe');
 
     // the iframe is styled such that it scrolls to focus on the schedule
-    iframe.style.width = "100%";
-    iframe.style.height = "1000%";
-    iframe.style.marginLeft = "-45px";
-    iframe.style.marginTop = "-300px";
-    iframe.style.overflow = "hidden";
-    iframe.style.pointerEvents = "none";
-    iframe.src = "https://acad.app.vanderbilt.edu/more/GetSchedule!input.action#scheduleTable";
+    iframe.style.width = '100%';
+    iframe.style.height = '1000%';
+    iframe.style.marginLeft = '-45px';
+    iframe.style.marginTop = '-300px';
+    iframe.style.overflow = 'hidden';
+    iframe.style.pointerEvents = 'none';
+    iframe.src = 'https://acad.app.vanderbilt.edu/more/GetSchedule!input.action#scheduleTable';
 
     // add the wrapper to the page and the iframe to the wrapper
     document.body.appendChild(wrapper);
-    document.getElementById("wrapper").appendChild(iframe);
+    document.getElementById('wrapper').appendChild(iframe);
   };
 });
