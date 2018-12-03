691e01d838f0ef709de6cd74bf457c731314baa6
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-12-03 14:37:31 -0600

    cleaned up some dead code

--- src/bg/enroll.js
+++ src/bg/enroll.js
@@ -3,14 +3,12 @@
 // https://github.com/sxlijin/assorted-code-snippets/blob/master/random-vandy-stuff/vandyClassRegistration.js
 
 // grab the user's classes
-const cart = document.getElementById('StudentCartList_div');
-const classes = cart.getElementsByClassName('classTable');
+const cart = document.getElementById("StudentCartList_div");
+const classes = cart.getElementsByClassName("classTable");
 
 for (let i = 0; i < classes.length; i++) {
   // find the current class
-  const classInfo = classes[i].getElementsByClassName('left')[0].childNodes;
-  const className = `${classInfo[1].innerText} ${classInfo[3].innerText}`;
-  const classSelection = classes[i].getElementsByClassName('classSelection')[0];
+  const classSelection = classes[i].getElementsByClassName("classSelection")[0];
 
   // grab the children of the current class
   const sub = classSelection.childNodes;
@@ -20,15 +18,15 @@ for (let i = 0; i < classes.length; i++) {
     // Clicking E▼ removes the "disabled" attribute from both, but does NOT toggle the waitListHidden input true.
     // Clicking W▼ does the same, but DOES toggle the waitListHidden input true.
     // By doing both, we enroll but also waitlist if the class if full.
-    sub[1].removeAttribute('disabled');
-    sub[3].removeAttribute('disabled');
-    sub[3].value = 'true';
+    sub[1].removeAttribute("disabled");
+    sub[3].removeAttribute("disabled");
+    sub[3].value = "true";
 
     // This accesses and modifies the dropdown text. While not necessary for this script to be
     // functional, that its it serves as
     // visual confirmation that the function has executed properly.
     const buttonText = sub[5].firstChild.firstChild.firstChild;
-    buttonText.textContent = 'W▼';
+    buttonText.textContent = "W▼";
   } catch (TypeError) {
     console.log(`Error during ${i}-th iteration:`);
     console.log(TypeError);
@@ -36,4 +34,4 @@ for (let i = 0; i < classes.length; i++) {
 }
 
 // click the enroll button
-document.getElementById('enrollButton-button').click();
+document.getElementById("enrollButton-button").click();
